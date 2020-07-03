96df3d46296bc7cafc748bb619f340d69fed80b2
AzizAlhaider <azizalhaider03@gmail.com>
2020-07-03 19:13:50 +0300

    Azizalhaider

---
+++ Development/js/scripts.js
@@ -0,0 +1,28 @@
+
+let mat=[1, 3 ,5 ,7]
+
+mat.forEach(logger)
+
+function logger(mat2){ 
+
+console.log(mat2)
+}
+   
+let degree=[40 , 45 , 39 , 20 , 25 , 35 ]
+
+
+
+console.log(degree.filter(hottestDays))
+
+function hottestDays(degree2){
+return degree2 < 35
+
+}
+
+let books=[ {name:"music 1" , id:123} ,{name:"music 2" , id:124} ,{name:"music 3" , id:125}]
+getBookById(books , 123)
+    function getBookById(books , id){
+    for(i of books)
+    }
+
+
